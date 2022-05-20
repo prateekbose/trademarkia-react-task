e149d4301e7c4a730a2d452a3f2ef69c8ab00203
prateekbose <prateekbose20011@gmail.com>
2022-05-20 13:45:06 +0530

    Added Invoice Template
    - Added Invoice Template

--- pages/Components/invoice.tsx
+++ pages/Components/invoice.tsx
@@ -25,13 +25,44 @@ export const Invoice: FunctionComponent<{}> = () => {
                         <h1>Transaction 1</h1>
                         <p>Paid on 05/04/2022</p>
                     </div>
-                    <div className="invoice amount">
+                    <div className="invoice-amount">
                         <h3>Amount Paid</h3>
                         <h1>$1,999.0</h1>
                     </div>
                 </div>
                 <div className="invoice-summary-content">
-                    
+                    <table>
+                        <tr>
+                            <th>Description</th>
+                            <th>Rate</th>
+                            <th>Qty</th>
+                            <th>Line Total</th>
+                        </tr>
+                        <tr>
+                            <td>Trademark Filing Application - USA</td>
+                            <td>$8,500.00</td>
+                            <td>1</td>
+                            <td>$8500.00</td>
+                        </tr>
+                        <tr>
+                            <td>Trademark Filing Application - USA</td>
+                            <td>$8,500.00</td>
+                            <td>1</td>
+                            <td>$8500.00</td>
+                        </tr>
+                        <tr>
+                            <td>Priority Rush Processing (Next Business Day)</td>
+                            <td>$89.00</td>
+                            <td>1</td>
+                            <td>$89.00</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>Total</td>
+                            <td>$1999.00</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
